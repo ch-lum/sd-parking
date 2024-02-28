2dde178947fc73cebcf32dc8300d75156880747c
Chris L <chrlum03@gmail.com>
2024-02-27 20:46:55 -0800

    Import npm and mapbox

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,5 +1,18 @@
 <script>
   // Write your JS here, or import other files
+  mapboxgl.accessToken = "pk.eyJ1IjoiY2gtbHVtIiwiYSI6ImNsc28yeHVhMDBiOTQya3BwejA5N2w5M24ifQ.goNJS9gKumvE34fkNDaW5g";
+	const map = new mapboxgl.Map({
+		container: "map",
+		style: "mapbox://styles/mapbox/outdoors-v12", 
+		center: [-71.0942, 42.3601], 
+		zoom: 13, // starting zoom level
+		minZoom: 12,
+		maxZoom: 15,
+	});
+
+  function project(d) {
+    return map.project(new mapboxgl.LngLat(+d.lon, +d.lat));
+  }
 </script>
 
 <main>
