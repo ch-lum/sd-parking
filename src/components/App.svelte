a11819e24e2bec19459f59c74ac085ad29843d72
Skrubstar <wangzilongcharles@gmail.com>
2024-02-28 18:06:54 -0800

    Added Parking Meters as GeoJSON and got Map working

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,23 +1,5 @@
 <script>
-  import mapbox from 'mapbox-gl';
-  import 'mapbox-gl/dist/mapbox-gl.css';
-  mapbox.accessToken = "pk.eyJ1IjoiY2gtbHVtIiwiYSI6ImNsc28yeHVhMDBiOTQya3BwejA5N2w5M24ifQ.goNJS9gKumvE34fkNDaW5g";
-	const map = new mapbox.Map({
-		container: "map",
-		style: "mapbox://styles/mapbox/outdoors-v12", 
-		center: [-71.0942, 42.3601], 
-		zoom: 13, // starting zoom level
-		minZoom: 12,
-		maxZoom: 15,
-	});
+    import Map from './Map.svelte';
 </script>
 
-<main>
-  <h1>Svelte template</h1>
-
-  <p>Write your HTML here</p>
-</main>
-
-<style>
-  /* Write your CSS here */
-</style>
+<Map />
