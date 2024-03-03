08ba1edc9b67020841152b149f973405e0ff0675
Chris L <chrlum03@gmail.com>
2024-03-02 21:27:23 -0800

    Redo Map

--- src/components/Map.svelte
+++ src/components/Map.svelte
@@ -1,86 +1,67 @@
 <script>
-  import mapbox from 'mapbox-gl';
-  import 'mapbox-gl/dist/mapbox-gl.css';
-  import { onMount } from 'svelte';
-  import * as d3 from 'd3';
-  let meter_data = []
-  let meter_markers = []
-  let map = []
-  
+  import mapbox from "mapbox-gl";
+  import "mapbox-gl/dist/mapbox-gl.css";
+  import { onMount } from "svelte";
+  import * as d3 from "d3";
+
+  let intervalId;
+
   onMount(() => {
     mapbox.accessToken = "pk.eyJ1IjoiY2gtbHVtIiwiYSI6ImNsc28yeHVhMDBiOTQya3BwejA5N2w5M24ifQ.goNJS9gKumvE34fkNDaW5g";
     const map = new mapbox.Map({
-        container: "map",
-        style: "mapbox://styles/mapbox/outdoors-v12",
-        center: [-117.16, 32.71],
-        zoom: 14.5, // starting zoom level
-        minZoom: 13,
-        maxZoom: 16,
-    });
+      container: "map",
+      style: "mapbox://styles/mapbox/light-v11",
+      center: [-117.16, 32.71],
+      zoom: 15.5,
+      minZoom: 13,
+      maxZoom: 16,
+    })
 
     let metersFile = "https://raw.githubusercontent.com/ch-lum/sd-parking/5ba586bc80c533d174a90a54f58b8c54ec95960a/static/parkingMeters.geojson";
-		
     fetch(metersFile)
-		.then((response) => response.json())
-		.then((d) => {meter_data = d.features})
-    .then((d) => create_meter_markers(meter_data));
-    
-   const marker_container = d3
-		.select(map.getCanvasContainer() )
-		.append("svg")
-		.attr("width", "100%")
-		.attr("height", "1280")
-    .style("position", "relative")
-		.style("z-index", 2);
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        map.on("load", () => {
+          map.addSource("meters", {
+            type: "geojson",
+            data: data,
+          });
+          map.addLayer({
+            id: "meters",
+            type: "circle",
+            source: "meters",
+            paint: {
+              "circle-color": "#0cf0af",
+              "circle-stroke-color": "#0c67f0",
+              "circle-stroke-width": 0.1,
+              "circle-opacity": 0.3,
+            },
+          });
 
-    function create_meter_markers(meter_data) {
-		  meter_markers = marker_container
-			.selectAll("circle")
-			.data(meter_data)
-			.enter()
-			.append("circle")
-			.style("fill", "#0cf0af")
-			.attr("stroke", "#0c67f0")
-			.attr("stroke-width", 0.1)
-			.attr("fill-opacity", 0.3);
-			position_meter_markers();
-          }
+          let currentFeatureIndex = 0;
+          intervalId = setInterval(() => {
+            let feature = data.features[currentFeatureIndex];
+            map.flyTo({ center: feature.geometry.coordinates, speed: 0.01 });
+            currentFeatureIndex = (currentFeatureIndex + 5) % data.features.length;
+          }, 2000); // Change the map's center every 5 seconds.
+        });
 
+        map.on("dragstart", () => {
+          clearInterval(intervalId); // Stop panning when the map is dragged.
+        });
+      });
 
-      function position_meter_markers() {
-		meter_markers
-			.attr("cx", function (d) {
-				return project(d.geometry.coordinates, 13).x
-			})
-			.attr("cy", function (d) {
-        return project(d.geometry.coordinates, 13).y
-      })
-      .attr("r", function (d) {
-        return map.getZoom()-12;
-      })
-    }
-
-    function project(d) {
-		let points = map.project(new mapboxgl.LngLat(+d[0], +d[1]));
-    return points
-	}
-  map.on("viewreset", position_meter_markers);
-	map.on("move", position_meter_markers);
-	map.on("moveend", position_meter_markers);
-	});
-
-
+  })
 </script>
 
-<main>
-  <h1>Parking Meter Map</h1>
-
-  <p>Zoom In For More Details</p>
-  <div id="map" style="width: 100%; height: 1280px;"></div>
-</main>
+<div id="map"></div>
 
 <style>
-  * {
-        text-align: center;
-    }
+  #map {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    width: 100%;
+  }
 </style>
