808523b5ec1a2f103c5f05030bf8a641a4d7bc1a
Chris L <chrlum03@gmail.com>
2024-03-02 23:50:21 -0800

    Add new chart roughly

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -2,6 +2,7 @@
   console.log('reset')
   import Map from './Map.svelte';
   import Cover from './Cover.svelte';
+  import Timeline from './Timeline.svelte';
 
   let showCover = true;
 
@@ -10,21 +11,46 @@
   }
 </script>
 
-<main>
+<main class="container">
   <Map />
-  {#if showCover}
-      <Cover>
-        <button on:click={toggleCover}>Explore Parking Meters</button>
-        <p><br>Or scroll<br><br>↓</p>
-      </Cover>
-  {/if}
-  
-  <button class="reset {!showCover ? 'visible' : ''}" on:click={toggleCover}>Reset</button>
+  <div class="wrapper">
+    {#if showCover}
+        <Cover>
+          <button on:click={toggleCover}>Explore Parking Meters</button>
+          <h2><br>Or scroll<br><br>↓</h2>
+        </Cover>
+    {/if}
+    
+    <button class="reset {!showCover ? 'visible' : ''}" on:click={toggleCover}>Reset</button>
+  </div>
+
+  <div class="first_text">
+    <p>San diego is well known for being a car-centric city. Blah blah blah blah. Here will go some introduction and discussion about cars</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <Timeline />
+  </div>
 </main>
 
 
 <style>
-  button, p {
+  .container {
+    display: flex;
+    flex-direction: column;
+    /* align-items: center;; */
+  }
+
+  h1 {
+    font-family: Georgia;
+    font-size: 4em;
+    text-align: center;
+  }
+
+  p {
+    font-family: Helvetica;
+    font-size: 1.5em;
+  }
+
+  button, h2 {
     font-family: Helvetica;
     font-size: 2.5em;
     color: rgba(95, 95, 95, 1);
@@ -36,10 +62,11 @@
     z-index: 3;
   }
 
-  p {
+  h2 {
     text-decoration: none;
     font-style: normal;
     margin: 0px;
+    font-weight: normal;
   }
 
   button:hover {
@@ -58,5 +85,15 @@
   button.reset.visible {
     opacity: 1;
     z-index: 3;
+  }
+
+  .first_text {
+    position: absolute;
+    top: 100%;
+    width: 100% - 400px;
+    border: 2px solid blue;
+    text-align: center;
+    padding-left: 200px;
+    padding-right: 200px;
   }
 </style>
