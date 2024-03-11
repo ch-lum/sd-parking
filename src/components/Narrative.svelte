e27e5c51a655c33ee4cf479bfd5baf6752689546
Chris L <chrlum03@gmail.com>
2024-03-10 20:58:30 -0700

    Narrative slides woop

--- src/components/Narrative.svelte
+++ src/components/Narrative.svelte
@@ -1,8 +1,12 @@
 <script>
     import { fade } from "svelte-transitions";
+    import Radial from "./Radial.svelte";
 
     export let page = 0;
-    export const lastPage = 6;
+    export const lastPage = 9;
+    export let data;
+    
+    const fadeDuration = 500;
 
     function nextPage() {
         page += 1;
@@ -16,14 +20,78 @@
     function skipToEnd() {
         page = lastPage;
     }
+
+
+    function makeRadial(days, areas, padres, scale, size) {
+        let subset = data.filter(d => days.includes(d.day_of_week) && areas.includes(d.area) && padres.includes(d.padres_today));
+
+        const newRadial = {
+            subset: subset,
+            params: {
+                selectedDay: days,
+                selectedArea: areas,
+                selectedPadres: padres,
+                scale: scale,
+                size: size,
+            },
+            key: rad_key
+        }
+
+        // radials = [...radials, newRadial];
+        rad_key++;
+        // console.log(radials);
+    }
 </script>
 
 <div id="narrative" in:fade={{ duration: 500 }} out:fade={{ duration: 500, css: (t) => `opacity: ${t*0}` }}>
     <div class="text">
-        <p class="page-num">{page} / {lastPage}</p>
+        <p class="page-num">{page} / {lastPage - 1}</p>
         <button class="previous {page === 0 ? 'disabled' : ''}" on:click={previousPage} disabled={page === 0}>Previous</button>
         <button class="next {page === lastPage ? 'disabled' : ''}" on:click={nextPage} disabled={page === lastPage}>Next</button>
         <button class="skip {page === lastPage ? 'disabled' : ''}" on:click={skipToEnd} disabled={page === lastPage}>Skip to end</button>
+        
+        {#if page===0}
+            <div in:fade={{ delay:fadeDuration, duration: fadeDuration }} out:fade={{ duration: fadeDuration }}>
+                <p>Start with overall radial. Explain what you're looking at--explain normalized (not linear yet)</p>
+            </div>
+        {:else if page === 1}
+            <div in:fade={{ delay:fadeDuration, duration: fadeDuration }} out:fade={{ duration: fadeDuration }}>
+                <p>Different locations get different colors. How they're different from each other. How this is a different version of the prior graph, but normalized. [EXPLAIN THIS]</p>
+            </div>
+        {:else if page === 2}
+            <div in:fade={{ delay:fadeDuration, duration: fadeDuration }} out:fade={{ duration: fadeDuration }}>
+                <p>We can also un-normalize. This is a monday for each of the locations.</p>
+            </div>
+        {:else if page === 3}
+            <div in:fade={{ delay:fadeDuration, duration: fadeDuration }} out:fade={{ duration: fadeDuration }}>
+                <p>Let's take a look at the East Village. [DESCRIBE EAST VILLAGE]. Petco park is here, home of the Padres. We can further look day-by-day at the totals. Here's the average weekday in the East Village and the average weekend. Weekends appear to have fewer people parking on average</p>
+            </div>
+        {:else if page === 4}
+            <div in:fade={{ delay:fadeDuration, duration: fadeDuration }} out:fade={{ duration: fadeDuration }}>
+                <p>If we split by day of week, we'll see that we weren't looking close enough. [EACH DAY OF THE WEEK FOR EAST VILLAGE]. Sundays just don't get any parking. If you have ever tried street parking yourself, you might know why (Hint: parking is free on Sundays).</p>
+            </div>
+        {:else if page === 5}
+            <div in:fade={{ delay:fadeDuration, duration: fadeDuration }} out:fade={{ duration: fadeDuration }}>
+                <p>In fact, here's all the locations and their average sunday. Unsurprisingly, there's nothing really there</p>
+            </div>
+        {:else if page === 6}
+            <div in:fade={{ delay:fadeDuration, duration: fadeDuration }} out:fade={{ duration: fadeDuration }}>
+                <p>Just for fun, here's the normalized versions of each.</p>
+            </div>
+        {:else if page === 7}
+            <div in:fade={{ delay:fadeDuration, duration: fadeDuration }} out:fade={{ duration: fadeDuration }}>
+                <p>Last observation, the Padres playing a home game does not really affect the number of people parking in San Diego. Here's the overall and the East Village, with the overlayed-versions in the center,.</p>
+            </div>
+        {:else if page === 8}
+            <div in:fade={{ delay:fadeDuration, duration: fadeDuration }} out:fade={{ duration: fadeDuration }}>
+                <p>Now, you're welcome to play with this tool on your own!</p>
+            </div>
+        {/if}
+    </div>
+    <div class="radial-box">
+        {#if page === 0}
+            <Radial {subset} {params} {uniqueId} />
+        {/if}
     </div>
 </div>
 
@@ -43,10 +111,13 @@
         padding: 20px;
         height: 23.5%;
         position: relative;
+        padding-left: 400px;
+        padding-right: 400px;
+        font-size: 1.4em;
     }
 
     button {
-        font-size: 1.2em;
+        font-size: 0.8em;
         color: rgba(95, 95, 95, 1);
         background-color: rgba(58, 58, 58, 0);
         border: 2px solid rgba(51, 51, 51, 0.3);
