3709768261d546a9d2213bcab806f402d1bf9409
ch-lum <32755060+ch-lum@users.noreply.github.com>
2024-03-13 01:14:23 -0700

    Add annotations

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -39,19 +39,71 @@
   </div>
 
   <div class="first_text">
-    <p>San Diego County has a population of 3.2 Million people, and if you've ever tried to go anywhere in rush hour before, you know that quite a sizeable proprotion of 
-    those people drive Though bonus fact, not as bad as it could since San Diego County is the 5th most populated, but only 18th on Traffic Severity, so it's not as bad as
-    it could be. I'm not sure that'll make the wait on the commute any easier though.
-    
-     But once you finally get downtown after a few hours of traffic, you have to do the one thing everyone dreads: Finding Parking. Or more accurately, finding street Parking
-     instead of shelling out large amounts of money to private parking lots. 
-     </p>
-     <p>
-     We'll be covering some interesting tidbits and insights, but feel more than free to explore on your 
-     own as well!
+    <p>
+      San Diego County has a population of 3.2 Million people, and if you've ever tried to go anywhere in rush hour before, you know that quite a sizeable proprotion of 
+      those people drive. Though bonus fact, not as bad as it could since San Diego County is the 5th most populated, but only 18th on Traffic Severity, so it's not as bad as
+      it could be. I'm not sure that'll make the wait on the commute any easier though.
+      
+      But once you finally get downtown after a long while of traffic, you have to do the one thing everyone dreads: Finding Parking. Or more accurately, finding street Parking
+      instead of shelling out large amounts of money to private parking lots. 
+      <br><br>
+      So, let's take a look at the parking meters in San Diego, and see if we can't find some patterns in the data. The San Diego Association of Governments (SANDAG) has wonderfully made publicly available ample amounts of data surrounding census, transportation, crime, and more for the San Diego region on their <a href="https://opendata.sandag.org/">open data portal</a>.
     </p>
-    <p>To understand when and where street parking gets used, we took a typical day in 2023, Thursday, March 16, and plotted the number of active parking meters over the course of the day.</p>
-    <Timeline />
+
+    <h3>The Meters</h3>
+    <p>
+      San Diego has 20 different neighborhoods with parking meters with nearly 5 million parking transactions in 2023 coming from 6,000 parking meters. 
+
+      To understand when and where street parking gets used, we took a typical day in 2023, Thursday, March 16, and plotted the number of active parking meters over the course of the day for each neighborhood. Further, the areas are ordered left-to-right the fewest maximum active meters to the most.
+    </p>
+    <div class="timeline">
+      <div class="timeline-comments">
+        <p class="annotation" style="top: 7%;">
+          The first people to begin parking start to pay at 6:00 AM when the meters first open. The Marina has attractions such as the USS Mideway and Seaport Village, so being at the heart of San Diego might explain their early start.
+        </p>
+
+        <p class="annotation" style="top: 15%; left: 10%;">
+          By 8:00 AM, the East Village, home of PetCo Park, has the most active meters, which comes as little surprise considering that they have the most parking meters in the city.
+        </p>
+
+        <p class="annotation" style="top: 22%; left: 40%;">
+          10:00 AM marks the first instance where a wave of meters end their transactions. Keep note of what times have quick drops like this one.
+        </p>
+
+        <p class="annotation" style="top: 29%; left: 48%;">
+          People arrive in Hillcrest before their noon errands and they cause a spike in active meters before many leave as the hour passes. Perhaps parking is easier if make plans for 12:05.
+        </p>
+
+        <p class="annotation" style="top: 36%; left: 48%;">
+          Some of the neighborhoods with the fewest meters, such as Point Loma, are far from downtown and only have a few meters. Though, Midtown, with a peak of 1 active meter, has 17 meters in total. Point Loma has the feweset with 8 meters.
+        </p>
+
+        <p class="annotation" style="top: 44%; left: 48%;">
+          Around 4:00 PM is a sort of parking meter happy hour, with a small lull in the number of total active meters. This is most prominent in Little Italy, which is known for their rich food scene explaining their resurgance around 5.
+        </p>
+        
+        <p class="annotation" style="top: 51%; left: 43%;">
+          At 6:00 PM, parking becomes free in most places. This is similar to how parking is free on holidays and Sundays, so we see a huge drop to zero transactions. The Marina in interesting, where only half of its transactions end.
+        </p>
+
+        <p class="annotation" style="top: 59%; left: 20%;">
+          The last of the meters end their transactions at 8:00 PM. Of the 20 locations, only Gaslamp seemed to manage to avoid the 6 PM drop and truly ends all their meters at 8.
+        </p>
+
+        <p class="annotation" style="top: 70%;">
+          The night is quiet. Street lights flicker overhead. Parking meters sleep a healthy ten hours each night before they wake back up and get back to it.
+        </p>
+
+        <p class="annotation" style="top: 82%;">
+          Despite the lack of parking meters, people are still welcome to park on the street. The lack of parking meters from 8 PM to 6 AM indicates a lack of charging instead of a lack of parking. Maybe the best time to park is at night?
+        </p>
+
+        <p class="annotation" style="top: 92%; width: 80%; font-size: 1em;">
+          [RECAP GAHHHHHH]
+        </p>
+      </div>
+      <Timeline />
+    </div>
   </div>
 
   {#if page !== lastPage}
@@ -64,6 +116,13 @@
     <Machine bind:data />
     <!-- <Radial /> -->
   </div>
+
+  <div class="bibliography">
+    <h3>References</h3>
+    <p>
+      <a href="https://opendata.sandag.org/">SANDAG Open Data Portal</a>
+      <a href="https://docs.sandiego.gov/municode/MuniCodeChapter08/Ch08Art06Division01.pdf">San Diego Municipal Code</a>
+    </p>
 
 </main>
 
@@ -83,8 +142,10 @@
 
   p {
     font-family: Helvetica;
-    font-size: 1.2em;
-    padding: 20px;
+    font-size: 1em;
+    padding-left: 60px;
+    padding-right: 60px;
+    text-align: justify;
   }
 
   button, h2 {
@@ -104,6 +165,15 @@
     font-style: normal;
     margin: 0px;
     font-weight: normal;
+  }
+
+  h3 {
+    font-family: Helvetica;
+    font-size: 1.4em;
+    font-weight: bold;
+    padding-left: 60px;
+    padding-right: 60px;
+    margin-top: 50px;
   }
 
   button:hover {
@@ -130,6 +200,7 @@
     width: 100%;
     /* border: 2px solid blue; */
     text-align: left;
+    margin-top: 100px;
     /* padding-left: 200px;
     padding-right: 200px; */
   }
@@ -139,7 +210,7 @@
     flex-wrap: wrap;
     position: absolute;
     top: 100%;
-    transform: translateY(3450px);
+    transform: translateY(3500px);
     width: 100%;
     /* border: 2px solid red; */
     text-align: left;
@@ -150,10 +221,35 @@
   .narrative {
     position: absolute;
     top: 100%;
-    transform: translateY(3450px);
+    transform: translateY(3500px);
     width: 100%;
     background-color: rgba(255, 255, 255, 0);
     z-index: 2;
     /* border: 2px dashed green; */
+  }
+
+  .timeline {
+    border: 2px solid blue;
+    z-index: 1;
+    background-color: rgba(255, 255, 255, 0);
+  }
+
+  .timeline-comments {
+    position: absolute;
+    left: 50%;
+    width: 50%;
+    height: 90%;
+    border: 2px solid green;
+    text-align: left;
+    z-index: 0;
+    padding: 0px;
+  }
+
+  .annotation {
+    position: absolute;
+    width: 50%;
+    padding: 0px;
+    font-size: 0.8em;
+    pointer-events: none;
   }
 </style>
